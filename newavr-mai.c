2078591c4b786d1431fd5792e02424542691dcd5
ahmed201511 <ahmed.saleh201511@gmail.com>
2021-09-11 16:32:22 +0200

    pass

---
+++ newavr-mai.c
@@ -0,0 +1,15 @@
+/*
+ * File:   newavr-mai.c
+ * Author: ahmed
+ *
+ * Created on 11 ??????, 2021, 04:17 ?
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
